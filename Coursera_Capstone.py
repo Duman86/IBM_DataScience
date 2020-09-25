21217ce72a03187a8e8fc2a3225bd3c94dec1180
Duman <31448147+Duman86@users.noreply.github.com>
2020-09-26 01:57:31 +0500

    Update Coursera_Capstone.py

--- Coursera_Capstone.py
+++ Coursera_Capstone.py
@@ -1,8 +1,6 @@
 //Capstone Project
 //This notebook will be used for doing the capstone project for IBM Data Science Professional Certificate
 
-@Author: Prasenjit Das
-
 Import numpy as np
 Import pandas as pd
 
